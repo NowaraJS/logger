cfbdee9e243def9990b0357c58d5a52fb0cebf6e
Komiroko <komiriko@pm.me>
2025-11-03 16:33:41 +0100

    fix(🔧): [use console level instead bun write for console sink]

--- source/sinks/console-logger.ts
+++ source/sinks/console-logger.ts
@@ -5,8 +5,9 @@ import type { LogLevels } from '#/types/log-levels';
 * ConsoleLoggerSink implements LoggerSink to provide logging functionality to the console.
 */
 export class ConsoleLoggerSink<TLogObject = unknown> implements LoggerSink<TLogObject> {
-	public async log(level: LogLevels, timestamp: number, object: TLogObject): Promise<void> {
+	public log(level: LogLevels, timestamp: number, object: TLogObject): void {
 		const logEntry = { timestamp, level, content: object };
-		await Bun.write(Bun.stdout, JSON.stringify(logEntry) + '\n');
+		const logLevel: Lowercase<LogLevels> = level.toLowerCase() as Lowercase<LogLevels>;
+		console[logLevel]?.(JSON.stringify(logEntry));
 	}
 }
