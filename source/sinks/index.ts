dd2a8c068227835b66edd46e7c7a4dce15d46d30
komiriko <komiriko@pm.me>
2026-01-13 12:38:41 +0100

    perf(⚡): [rewrite FileLoggerSink with Bun.FileSink]

--- source/sinks/index.ts
+++ source/sinks/index.ts
@@ -1,5 +1,4 @@
 export { ConsoleLoggerSink } from './console-logger';
 export { DevNullLoggerSink } from './devnull-logger';
-export { FILE_LOGGER_ERROR_KEYS } from './file-logger/enums/file-logger-error-keys';
-export { FileLoggerSink, type FileLoggerConfig } from './file-logger/file-logger';
+export { FileLoggerSink } from './file-logger';
 
